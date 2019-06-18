77a1fbba6d6687c62427e119bd552f31632a6302
Michael <michael.sauthier@umontreal.ca>
2019-06-18 10:55:06 -0400

    v0.2.5 documentation

---
+++ R/BlandAltman.R
@@ -0,0 +1,19 @@
+#' Bland-Altman: Bland-Altman Analysis and Plot
+#'
+#' This package calculated the Bland-Alman analysis and generates the plot.
+#' It also computed specific and overall agreement.
+#'
+#' Please see the documentation for BA.analysis(), BA.plot() and Agreement()
+#'
+#' References:
+#'
+#' 1) Bland, J.M. & Altman, D.G., 1986. Statistical methods for assessing agreement
+#' between two methods of clinical measurement. Lancet, 1(8476), pp.307–310.
+#'
+#' 2) Giavarina, D., 2015. Understanding Bland Altman analysis. Biochemia Medica, 25(2), pp.141–151
+#'
+#' @docType package
+
+NULL
+
+
